dfc02932386a706460e5a539e570ff9ea864527e
Louka Ewington-Pitsos <lewingtonpitsos@gmail.com>
2024-06-23 14:19:11 +1000

    show runpod port info

--- app/cli.py
+++ app/cli.py
@@ -31,15 +31,15 @@ def handle_train(args):
     print('ʕノ•ᴥ•ʔノ\ntraining complete, hopefully nothing went horribly wrong...\n')
 
 def handle_runpod(args):
-    if args.action == 'create':
-        create_pod()
+    if args.action == 'deploy':
+        deploy_pod()
     elif args.action == 'purge':
         print('ʕノ•ᴥ•ʔノ\npurging all pods...\n')
         destroy_all_pods()
     elif args.action == 'show':
         show_pods()
 
-    print("°˖✧◝(⁰▿⁰)◜✧˖°")
+    print("\n°˖✧◝(⁰▿⁰)◜✧˖°")
 
 def main():
     parser = argparse.ArgumentParser(description='CLI for various tools.')
@@ -59,7 +59,7 @@ def main():
 
     # Runpod tool
     parser_runpod = subparsers.add_parser('runpod', help='Create a Runpod')
-    parser_runpod.add_argument('action', choices=['create', 'purge', 'show'], help='Action to perform')
+    parser_runpod.add_argument('action', choices=['deploy', 'purge', 'show'], help='Action to perform')
     parser_runpod.set_defaults(func=handle_runpod)
 
     args = parser.parse_args()
--- app/rp.py
+++ app/rp.py
@@ -3,8 +3,7 @@ import uuid
 
 from app.constants import *
 
-
-def create_pod(gpu_type_id="NVIDIA A100 80GB PCIe"):
+def deploy_pod(gpu_type_id="NVIDIA A100 80GB PCIe"):
     runpod.api_key = CRED['RUNPOD_API_KEY']
 
     env = {**CRED}
@@ -12,10 +11,12 @@ def create_pod(gpu_type_id="NVIDIA A100 80GB PCIe"):
     pod = runpod.create_pod(
         cloud_type="SECURE", # or else someone might snoop your session and steal your AWS/CDS credentials
         name=str(uuid.uuid4())[:32], 
-        image_name="unpod/pytorch:2.2.0-py3.10-cuda12.1.1-devel-ubuntu22.04", 
+        image_name="runpod/pytorch:2.2.0-py3.10-cuda12.1.1-devel-ubuntu22.04", 
         gpu_type_id=gpu_type_id,
         container_disk_in_gb=400,
-        env=env
+        env=env,
+        ports="22/tcp"
+
     )
 
     print(f"Pod created: {pod}")
@@ -28,6 +29,17 @@ def show_pods():
     print(f"Total pods: {len(pods)}")
     for pod in pods:
         print(pod)
+        port_data = pod['runtime']['ports'][-1]
+        print(port_data)
+        print(f"""\nHost runpod.io
+    HostName {port_data['ip']}
+    User root
+    Port {port_data['publicPort']}
+    IdentityFile ~/.ssh/id_louka
+    IdentitiesOnly yes
+""")
+
+
 
 def destroy_all_pods():
     runpod.api_key = CRED['RUNPOD_API_KEY']
