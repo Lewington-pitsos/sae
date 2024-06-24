52dcff49e4e30d87ae1b9bffdb1f468904eaa67f
Louka Ewington-Pitsos <lewingtonpitsos@gmail.com>
2024-06-24 16:36:42 +1000

    get ingestion working correctly

--- app/ingest.py
+++ app/ingest.py
@@ -1,65 +1,61 @@
-import torch
-from tqdm import tqdm
 from datasets import load_dataset, get_dataset_config_names
 from datasets import Dataset, DatasetDict, Features, ClassLabel, Value
 
-
 from app.constants import *
 
-def ingest_ade_raft():
-    raft_ade = load_dataset("ought/raft", 'ade_corpus_v2', cache_dir="cache")
 
-    ade = load_dataset('ade-benchmark-corpus/ade_corpus_v2', 'Ade_corpus_v2_classification')
+todo = {
+    # 'neurips_impact_statement_risks': None,
+    'tweet_eval_hate': 'cardiffnlp/tweet_eval', # 'hate'
+    # 'overruling',
+    # 'semiconductor_org_types',
+    # 'tai_safety_research',
+    # 'terms_of_service',
+    # 'twitter_complaints',
+}
 
-
-    ade_dict = {row['text']: row['label'] for row in ade['train']}
-
-    label_mapping ={
-        0: 2,
-        1: 1,
-    }
-
-    # Function to add a new column 'label2'
-    def add_matching_label(example):
-        text = example['Sentence']
-        # Check if the text matches any text in the ade_dict
-        example['label'] = label_mapping[ade_dict.get(text, None)]  # Adds None if no match found
-        
-
-        return example
-
-    # Apply the function to both train and test sets of raft_subset
-    raft_subset = raft_ade.map(add_matching_label)
-
-    # Example: print the updated raft_subset
-    for sample in raft_subset['train']:
+def validate_new_dataset(ds, expected_test_len):
+    label_counts = {0: 0, 1: 0, 2: 0}
+    for sample in ds['train']:
         assert sample['label'] is not None
         assert sample['label'] == sample['Label']
+        label_counts[sample['label']] += 1
 
-    for sample in raft_subset['test']:
+    for label, count in label_counts.items():
+        print(f"train {label}: {count}, {count / len(ds['train'])}")
+
+    for sample in ds['test']:
         assert sample['label'] is not None
 
+    for label, count in label_counts.items():
+        print(f"test {label}: {count}, {count / len(ds['train'])}")
 
+
+    assert len(ds['train']) == 50, f"train size {len(ds['train'])}"
+    assert len(ds['test']) == expected_test_len, f"test size {len(ds['test'])}"
+
+
+def reformat(example, text_name):
     label_remapping = {2: 0, 1: 1}
+    example['text'] = example[text_name]
+    example['label'] = label_remapping[example['label']]
+
+    del example[text_name]
+    del example['ID']
+    del example['Label']
+
+    return example
 
 
-    def reformat(example):
-        example['text'] = example['Sentence']
-        example['label'] = label_remapping[example['label']]
+def save_dataset(raft, name):
+    dataset_name = f'{LOCAL_DATA_PATH}/raft_{name}'
 
-        del example['Sentence']
-        del example['ID']
-        del example['Label']
+    print('saving to:', dataset_name)
 
-        return example
-    
-    raft_subset = raft_subset.map(reformat)
-
-    # Assuming each item in raft_subset['train'] and raft_subset['test'] has 'data' and 'label'
-    train_dataset = Dataset.from_dict({'text': [item['text'] for item in raft_subset['train']],
-                                    'label': [item['label'] for item in raft_subset['train']]})
-    test_dataset = Dataset.from_dict({'text': [item['text'] for item in raft_subset['test']],
-                                    'label': [item['label'] for item in raft_subset['test']]})
+    train_dataset = Dataset.from_dict({'text': [item['text'] for item in raft['train']],
+                                    'label': [item['label'] for item in raft['train']]})
+    test_dataset = Dataset.from_dict({'text': [item['text'] for item in raft['test']],
+                                    'label': [item['label'] for item in raft['test']]})
 
 
     # Optionally specify features if your dataset needs specific types
@@ -79,6 +75,59 @@ def ingest_ade_raft():
     })
 
     # Save to disk
-    dataset_dict.save_to_disk(F'{LOCAL_DATA_PATH}/ade_raft_labelled')
+    dataset_dict.save_to_disk(dataset_name)
 
-ingest_ade_raft()
+def ingest_raft(raft_subset_name, raft_text_name, dataset, dataset_subset):
+
+    label_data = load_dataset(dataset, dataset_subset)
+    raft = load_dataset("ought/raft", raft_subset_name, cache_dir="cache")
+    
+    expected_test_len = len(raft['test'])
+
+    text_dict = dict()
+    mis_labelled = []
+    duplicates = []
+    for split in [k for k in label_data.keys() if k in ['train', 'test']]:
+        print(split)
+        for row in label_data[split]:
+            if row['text'] in text_dict:
+                if text_dict[row['text']] != row['label']:
+                    print(f"Inconsistent label on: {row['text']}, {row['label']}")
+
+                    mis_labelled.append(row['text'])
+                duplicates.append(row['text'])
+            text_dict[row['text']] = row['label']
+
+
+    print(f"Found {len(duplicates)} duplicates")
+    for dupe in list(set(duplicates))[:10]:
+        print('duplicate: ', dupe)
+
+    print(f"Found {len(mis_labelled)} duplicates with mismatched labels")
+
+    label_mapping ={
+        0: 2,
+        1: 1,
+    }
+
+    def add_matching_label(example):
+        text = example[raft_text_name]
+        # Check if the text matches any text in the ade_dict
+        
+        if text in mis_labelled and example['Label'] != 0:
+            example['label'] = example['Label']
+        else:
+            example['label'] = label_mapping[text_dict.get(text, None)]  # Adds None if no match found
+        
+
+        return example
+
+    raft = raft.map(add_matching_label)
+
+    validate_new_dataset(raft, expected_test_len)
+    raft = raft.map(lambda x: reformat(x, raft_text_name))
+
+    save_dataset(raft, raft_subset_name)
+
+ingest_raft('tweet_eval_hate', 'Tweet', 'cardiffnlp/tweet_eval', 'hate')
+ingest_raft('ade_corpus_v2', 'Sentence', 'ade-benchmark-corpus/ade_corpus_v2', 'Ade_corpus_v2_classification')
